c6f3602031908ec008f596ebee389274449e8fa5
Pawel <pawelek4321111@gmail.com>
2021-04-20 16:48:02 +0200

    initial commit

---
+++ src/main/java/io/github/Vendeis/allegrotask/AllegrotaskApplication.java
@@ -0,0 +1,13 @@
+package io.github.Vendeis.allegrotask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllegrotaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllegrotaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/Vendeis/allegrotask/AllegrotaskApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Vendeis.allegrotask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllegrotaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
